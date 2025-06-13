13c33ff6a9c1e1bdb1a39c4eb2567fa85a8c89cf
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-06-13 09:30:09 +0500

    Bajarildi

---
+++ practice11.py
@@ -0,0 +1,17 @@
+def calculate_tax(salary):
+    if salary > 5_000_000:
+        tax = salary * 0.2
+    else:
+        tax = salary * 0.13
+    return tax
+
+def calculate_net_salary(salary, tax):
+    net_salary = salary - tax
+    return net_salary
+
+salary = int(input( "Summa kriting: "))
+tax = calculate_tax(salary)
+print(tax)
+
+net_salary = calculate_net_salary(salary, tax)
+print(net_salary)
